1224b04eb51a6bb001aa92c1027b493bd6c248d8
Aniket Datta <datta.aniket7@gmail.com>
2023-10-10 04:05:52 +0530

    button

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,22 @@
 import Nav from "@/components/Nav";
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="p-24">
       <Nav />
+      <section className="py-12 flex flex-col items-center text-center gap-8">
+        <h1 className="text-4xl font-bold">Shadcn is awesome</h1>
+        <p className="text-2xl text-muted-foreground">
+          {" "}
+          Hand-picked themes that you can copy amd paste into your apps.
+        </p>
+      </section>
+      <div className="flex gap-6 items-center justify-center">
+        <Button>Learn more</Button>
+        <Button>Enroll now</Button>
+      </div>
     </main>
   );
 }
